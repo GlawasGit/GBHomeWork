d10d437140c86d49b35378c34543c15f16dc3b43
GlawasGit <83542451+GlawasGit@users.noreply.github.com>
2021-05-22 10:14:16 +0300

    add homework

---
+++ Tasck2.cs
@@ -0,0 +1,58 @@
+﻿//using System;
+
+//namespace Q1L2_2
+//{
+//    class Tasck2
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Введите число от 1 до 12");
+//            int month = Convert.ToInt32(Console.ReadLine());
+//            string monNum = "";
+//            switch (month)
+//            {
+//                case 0:
+//                case 1:
+//                    monNum = "Январь";
+//                    break;
+//                case 2:
+//                    monNum = "Февраль";
+//                    break;
+//                case 3:
+//                    monNum = "Март";
+//                    break;
+//                case 4:
+//                    monNum = "Апрель";
+//                    break;
+//                case 5:
+//                    monNum = "Май";
+//                    break;
+//                case 6:
+//                    monNum = "Июнь";
+//                    break;
+//                case 7:
+//                    monNum = "Июль";
+//                    break;
+//                case 8:
+//                    monNum = "Август";
+//                    break;
+//                case 9:
+//                    monNum = "Сентябрь";
+//                    break;
+//                case 10:
+//                    monNum = "Октябрь";
+//                    break;
+//                case 11:
+//                    monNum = "Ноябрь";
+//                    break;
+//                case 12:
+//                    monNum = "Декабрь";
+//                    break;
+//                default:
+//                    Console.WriteLine("Fatal Eror!");
+//                    break;
+//            }
+//            Console.WriteLine(monNum);
+//        }
+//    }
+//}
---
+++ Tasck4.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+
+//namespace Q1L2_4_
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string comname = "ООО Звезда";
+//            string product = "Майка розовая 44";
+//            string product1 = "Майка белая 46";
+//            string product2 = "Майка желтая 46";
+//            double productsum = 5487.10;
+//            int productsum1 = 1425;
+//            int productsum2 = 2821;
+//            double totalsum = 9733.10;
+//            int cash = 10000;
+//            double change = 266.90;
+//            DateTime date = new DateTime(2013, 6, 29, 12, 14, 00);
+//            Console.WriteLine($"**************{comname}**************");
+//            Console.WriteLine("************КПП 110001001*************");
+//            Console.WriteLine("*ККМ 00044444**ИНН 001111111111 #0006*");
+//            Console.WriteLine($"*{date}****АДМИНИСТРАТОР*");
+//            Console.WriteLine("*ПРОДАЖА************************#0001*");
+//            Console.WriteLine($"*{product}*************={productsum}*");
+//            Console.WriteLine($"*{product1}*****************={productsum1}*");
+//            Console.WriteLine($"*{product2}****************={productsum2}*");
+//            Console.WriteLine($"*ИТОГО************************={totalsum}*");
+//            Console.WriteLine($"*НАЛИЧНЫМИ*********************={cash}*");
+//            Console.WriteLine($"*СДАЧА*************************={change}*");
+//            Console.WriteLine("******************ФП******************");
+//            Console.WriteLine("************ЭКЛЗ 3856444498***********");
+//            Console.WriteLine("***********00023697 #062117***********");
+            
+//            Console.ReadKey();
+//        }
+//    }
+//}
