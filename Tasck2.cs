d054e121342f6cf6535a161c518a6c4deff73609
GlawasGit <83542451+GlawasGit@users.noreply.github.com>
2021-05-26 17:41:02 +0300

    Add files via upload

---
+++ Tasck2.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+
+//namespace ConsoleApp5
+//{
+//    class Tasck1
+//    {
+//        static void Main(string[] args)
+//        {
+//            string[,] phonebook = new string[5, 2]
+//          {
+//              {"Петр Петрович","504 33 88"},
+//              {"Василий Васийливич","567 66 31"},
+//              {"Иван Иванович","532 72 49"},
+//              {"Михаил Михайлович","590 74 89"},
+//              {"Сергей Сергеевич","555 41 09"}
+//          };
+//            for (int x = 0; x < phonebook.GetLength(0); x++)
+//            {
+//                for (int y = 0; y < phonebook.GetLength(1); y++)
+//                {
+//                    Console.Write($"{phonebook[x, y]} ");
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
