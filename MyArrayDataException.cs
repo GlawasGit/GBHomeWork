000ff558b5c6a600fb88f01c3c94ba641f712396
GlawasGit <83542451+GlawasGit@users.noreply.github.com>
2021-06-05 13:21:42 +0300

    add home work

---
+++ MyArrayDataException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ConsoleApp8
+{
+    [Serializable]
+    internal class MyArrayDataException : Exception
+    {
+        int row, column;
+
+        public MyArrayDataException(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+    }
+}
